0a04ced68a58d826e704cabf9d296f76bcc0496e
Daniel Shin <clickdaniel@mac.com>
2021-01-19 22:25:21 +0900

    구입기록 테이블뷰 컨트롤러 추가

---
+++ FreshFridge/FreshFridge/PurchaseRecordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PurchaseRecordTableViewCell.swift
+//  FreshFridge
+//
+//  Created by iM27 on 2021/01/19.
+//
+
+import UIKit
+
+class PurchaseRecordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
